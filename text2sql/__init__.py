642be11b5d6031215ab7f4257a3b3939ff21b6b4
Sarthak Doshi <115373283+SdSarthak@users.noreply.github.com>
2026-07-31 06:19:33 +0530

    set package author

--- text2sql/__init__.py
+++ text2sql/__init__.py
@@ -1,7 +1,7 @@
 """
-Text-to-SQL LLM Application Package
+Text-to-SQL LLM application package.
 """
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
-__description__ = "Convert natural language queries to SQL using LLMs"
+__author__ = "Sarthak Doshi"
+__description__ = "Convert natural language queries to SQL against Databricks"
